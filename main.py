1ff618d5bf204e4dcc410e4e871da7c7de4dc6ca
aandrks <a.andrks@gmail.com>
2025-12-22 22:35:06 +0300

    Update main.py
    added more iata codes

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ url = f"https://api.travelpayouts.com/aviasales/v3/grouped_prices?origin={origin
 # print(dict_result['data'])
 # for s in (dict_result['data']):
 #     print(s)
-flights_directions = ['UUS']
+flights_directions = ['IST', 'SAW', 'AYT', 'DXB', 'AUH', 'EVN', 'MSQ', 'TAS', 'ALA', 'BKK', 'HKT', 'SGN', 'AER', 'MRV', 'KGD', 'LED', 'SVX', 'KZN', 'VVO', 'KHV', 'UUS']
 
 
 def define_timedate(d):
